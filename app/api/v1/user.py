9a6e1a8261401a4321d66cc5cdc0b84bef24dacc
Alexcuke <alexcuke@gmail.com>
2026-05-26 13:01:11 +0300

    new

---
+++ app/api/v1/user.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter,Depends
+from sqlalchemy.orm import Session
+from  app.dependency import get_db
+from  app.schemas import UserRequest,UserResponse
+from app.service import users as users_service
+router=APIRouter
+
+@router.post("/users",response_model=UserResponse)
+def create_user(payload:UserRequest,db: Session=Depends(get_db)):
+    return users_service.create_user(db, payload.login)
